ff55b9be51986f8a4512a14e7fce5e87ac4078ed
JuanDr08 <156454177+JuanDr08@users.noreply.github.com>
2024-03-26 18:37:08 -0500

    feat: :building_construction: Linked html with flask
    linked all the screens of the pages using flask blocks

--- flaskFunct.py
+++ flaskFunct.py
@@ -7,8 +7,23 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route("/abrigos")
+@app.route("/views/abrigos")
 def abrigos():
     return render_template('abrigos.html')
 
+@app.route("/views/camisetas")
+def tshirts():
+    return render_template('camisetas.html')
+
+@app.route("/views/pantalones")
+def pants():
+    return render_template('pantalones.html')
+
+@app.route("/cart")
+def cart():
+    return render_template('carritos.html')
+
+@app.route("/emptcart")
+def emptcart():
+    return render_template('carritoVacio.html')
 app.run(debug=True)
