ccbb86b12c4914dc2941b289bb9964d9709025f7
JuanDr08 <156454177+JuanDr08@users.noreply.github.com>
2024-03-26 20:24:46 -0500

    feat: :sparkles: Finished Flask web
    Finished the web with jinja flask by creating the mockapi and calling it to the .py and using for loops ton add the products to the html

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Mostly Fluid Responsive Design
 
 # Flask Framework implementation
+
+
+
+> Para el desarrollo del ecommerce con flask utilicé propiedades diferentes a las enseñadas en clase, como los filters, blocks, route, condiciones, extends y llamados a funciones del .py, ademas de funciones internas de jinja, esto lo aprendi leyendo la documentacion oficial de jinja
+>
+> [Documentacion Jinja]: https://jinja.palletsprojects.com/en/3.1.x/templates/
--- flaskFunct.py
+++ flaskFunct.py
@@ -1,29 +1,32 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
+peticion = requests.get("https://660347162393662c31cecf3e.mockapi.io/api/v1/Products").json()
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('index.html', peticion=peticion)
 
 @app.route("/views/abrigos")
 def abrigos():
-    return render_template('abrigos.html')
+    return render_template('abrigos.html', peticion=peticion)
 
 @app.route("/views/camisetas")
 def tshirts():
-    return render_template('camisetas.html')
+    return render_template('camisetas.html', peticion=peticion)
 
 @app.route("/views/pantalones")
 def pants():
-    return render_template('pantalones.html')
+    return render_template('pantalones.html', peticion=peticion)
 
 @app.route("/cart")
 def cart():
-    return render_template('carritos.html')
+    return render_template('carritos.html', peticion=peticion)
 
 @app.route("/emptcart")
 def emptcart():
     return render_template('carritoVacio.html')
+
 app.run(debug=True)
