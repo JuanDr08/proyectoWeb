492d7a64157f3d0edebec1ee7bbb2c267fb4ab55
JuanDr08 <156454177+JuanDr08@users.noreply.github.com>
2024-05-16 17:21:07 -0500

    refactor: :recycle: Improving the JS code
    Deleting innecesary code to start improving it

---
+++ js/components/cards.js
@@ -0,0 +1,33 @@
+
+//Estructura card comun
+`<div>
+    <img src="storage/img/blackJacket.webp" alt="chaqueta 1">
+    <div class="informacion">
+        <p>black leather jacket</p>
+        <p class="precio">$100000</p>
+        <button onclick="botones(1)">Buy</button>
+    </div>
+</div>`
+
+//Estructura card carrito
+
+`
+<div>
+    <img src="storage/img/omegaHoodie.webp" alt="chaqueta 5">
+    <div>
+        <div class="informacion_cart">
+            <p>Black formal jacket</p>
+            <p class="precio">$160000</p>
+        </div>
+        <div class="cantidad">
+            <p>Cantidad</p>
+            <p>1</p>
+        </div>
+        <div class="sub">
+            <p>Sub-Total</p>
+            <p>$ 160000</p>
+        </div>
+    </div>
+    <button onclick="borrar()" class="eliminar"><i class='bx bx-trash'></i></button>
+</div>
+`
